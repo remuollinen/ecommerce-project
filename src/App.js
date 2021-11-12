2410d66656a96334b6932c2af6819f856f3cbeb4
Ouss84 <bahrioussama84@gmail.com>
2021-11-12 12:32:58 +0200

    Added the main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-			<h1>Welcome to our eCommerce website</h1>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <h1>Welcome to our eCommerce website</h1>
+      <Main />
+    </div>
+  );
 }
 
 export default App;

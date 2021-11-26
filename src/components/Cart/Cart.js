4b299a8ab2244fe6ff868259c162e2d0d0c4285a
Ouss84 <bahrioussama84@gmail.com>
2021-11-26 15:47:21 +0200

    adding the remove from cart functionality and initial styling of cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,32 +1,45 @@
 import React from "react";
 import "./Cart.css";
 const Cart = (props) => {
-  const { cartItems, addItem } = props;
+  const { cartItems, addItem, removeItem } = props;
+  const subTotal = cartItems.reduce(
+    (actual, cummulator) => actual + cummulator.price * cummulator.quantity,
+    0
+  );
+  // console.log(subTotal);
   return (
-    <div>
-      <h2>Cart</h2>
-      <div className="cart-container">
-        {cartItems.length === 0 && (
-          <div>
-            <p>Cart is empty</p>
-          </div>
-        )}
-        {cartItems.map((item) => (
-          <div key={item.id}>
-            <div>{item.title}</div>
-            <div>{item.price} $</div>
-            <div>{item.quantity}</div>
-            <div>
-              <button onClick={() => addItem(item)}>+</button>
-              {/* <button onClick={() => removeItem(item)}>-</button> */}
-            </div>
+    <div className="cart-container">
+      {cartItems.length === 0 && (
+        <div>
+          <p>Cart is empty</p>
+        </div>
+      )}
+      <div className="items-container">
+        <h2>Cart</h2>
+        <div className="items">
+          {cartItems.map((item) => (
+            <div key={item.id} className="item-details">
+              <div className="image-title-wrapper">
+                <div className="item-title">{item.title}</div>
+                <img src={item.image} alt={item.title}></img>
+              </div>
 
-            <div>
-              {item.quantity} X {item.price} $
+              <div className="price-quantity">
+                <span>{item.quantity}</span> x <span>{item.price}</span> $
+                <div className="add-Button">
+                  <button onClick={() => addItem(item)}>+</button>
+                  <button onClick={() => removeItem(item)}>-</button>
+                </div>
+              </div>
             </div>
-            {/* {console.log(item.quantity)} */}
-          </div>
-        ))}
+          ))}
+        </div>
+      </div>
+      <div className="sub-total">
+        {cartItems.length !== 0 && <div> Your Sub-total : {subTotal}</div>}
+        <div className="go-to-payment">
+          <button> Go to payment </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { BiEnvelope } from "react-icons/bi";
 import { BsInstagram, BsFacebook, BsPaypal } from "react-icons/bs";
@@ -6,73 +6,73 @@ import { SiVisa, SiKlarna, SiGooglepay } from "react-icons/si";
 import { FaApplePay } from "react-icons/fa";
 
 const Footer = () => {
-        return (
-            <div>
-                <div className="contacts-wrapper">
-                <div className="contacts-header">
-                    <h2>Contacts</h2>
-                </div>
-                    <nav className="footer-nav">
-                        <ul>
-                            <li>
-                                <NavLink to="contact-info">Contact info</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="disclaimer">Disclaimer</NavLink >
-                            </li>
-                            <li>
-                                <NavLink  to="returns">Return policy</NavLink >
-                            </li>
-                        </ul>
-                    </nav>
-                    <div className="some-wrapper">
-                        <nav>
-                            <ul>
-                                <li>
-                                    <NavLink  to="Instagram">
-                                    <BsInstagram className="some-icon" />
-                                    </NavLink >
-                                </li>
-                                <li>
-                                    <NavLink  to="Facebook">
-                                    <BsFacebook className="some-icon" />
-                                    </NavLink >
-                                </li>
-                                <li>
-                                    <NavLink  to="Mail" >
-                                    <BiEnvelope className="some-icon" />
-                                    </NavLink >
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                        <div className="payment-wrapper">
-                            <nav>
-                                <ul>
-                                    <li>
-                                        <SiVisa className="payment-icon" />
-                                    </li>
-                                    <li>
-                                        <SiKlarna className="payment-icon" />
-                                    </li>
-                                    <li>
-                                        <SiGooglepay className="payment-icon" />
-                                    </li>
-                                    <li>
-                                        <BsPaypal className="payment-icon" />
-                                    </li>
-                                    <li>
-                                        <FaApplePay className="payment-icon" />
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                <p className="copyright"><small>Copyright &copy; The GitMen 2021</small></p>
-                </div>
-            </div>
-            
-        );
-    }
-
+  return (
+    <div>
+      <div className="contacts-wrapper">
+        <div className="contacts-header">
+          <h2>Contacts</h2>
+        </div>
+        <nav className="footer-nav">
+          <ul>
+            <li>
+              <NavLink to="contact-info">Contact info</NavLink>
+            </li>
+            <li>
+              <NavLink to="disclaimer">Disclaimer</NavLink>
+            </li>
+            <li>
+              <NavLink to="returns">Return policy</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="some-wrapper">
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="Instagram">
+                  <BsInstagram className="some-icon" />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="Facebook">
+                  <BsFacebook className="some-icon" />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="Mail">
+                  <BiEnvelope className="some-icon" />
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="payment-wrapper">
+          <nav>
+            <ul>
+              <li>
+                <SiVisa className="payment-icon" />
+              </li>
+              <li>
+                <SiKlarna className="payment-icon" />
+              </li>
+              <li>
+                <SiGooglepay className="payment-icon" />
+              </li>
+              <li>
+                <BsPaypal className="payment-icon" />
+              </li>
+              <li>
+                <FaApplePay className="payment-icon" />
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <p className="copyright">
+          <small>Copyright &copy; The GitMen 2021</small>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;

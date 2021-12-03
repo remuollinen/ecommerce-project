c40738aa5692f64a4569a70f4666a74ba568fbe3
Remu Ollinen <remu.ollinen@gmail.com>
2021-12-03 13:19:03 +0200

    added routing

--- src/components/LandingPage/items.js
+++ src/components/LandingPage/items.js
@@ -1,9 +1,7 @@
-import ProductsList from "../AllProducts/ProductsList";
-
 export let items = [
-  { id: 1, category: "phones", price: 1000, brand: "iphone" },
-  { id: 2, category: "cars", price: 18000, brand: "bmw" },
-  { id: 3, category: "laptops", price: 750, brand: "dell" },
-  { id: 4, category: "TVs", price: 250, brand: "tv-samsoung" },
-  { id: 5, category: "shoes", price: 50, brand: "adidas-shoes" },
+	{ id: 1, category: "phones", price: 1000, brand: "iphone" },
+	{ id: 2, category: "cars", price: 18000, brand: "bmw" },
+	{ id: 3, category: "laptops", price: 750, brand: "dell" },
+	{ id: 4, category: "TVs", price: 250, brand: "tv-samsoung" },
+	{ id: 5, category: "shoes", price: 50, brand: "adidas-shoes" },
 ];

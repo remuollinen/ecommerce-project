fe276c06d59e737a5620370762c1b501269a628a
Ouss84 <bahrioussama84@gmail.com>
2021-12-04 16:42:12 +0200

    adding navigation button to the cart+styling

--- src/App.js
+++ src/App.js
@@ -4,104 +4,104 @@ import Main from "./components/LandingPage/Main";
 import ProductsList from "./components/AllProducts/ProductsList";
 import Footer from "./components/Footer";
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useParams,
-	useNavigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+  useNavigate,
 } from "react-router-dom";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
 
 function RouteWrapper(props) {
-	const params = useParams();
-	const navigate = useNavigate();
-	return <SingleProduct params={params} {...props} navigate={navigate} />;
+  const params = useParams();
+  const navigate = useNavigate();
+  return <SingleProduct params={params} {...props} navigate={navigate} />;
 }
 
 const App = () => {
-	const [cartItems, setCartItems] = useState([
-		// for testing purposes, remove the object and leave the array empty
-		{
-			id: 3,
-			image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-			price: 55.99,
-			rating: { rate: 4.7, count: 500 },
-			title: "Mens Cotton Jacket",
-			quantity: 1,
-		},
-		{
-			id: 2,
-			image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-			price: 55.99,
-			rating: { rate: 4.7, count: 500 },
-			title: "Mens Cotton Jacket",
-			quantity: 1,
-		},
-		{
-			id: 1,
-			image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-			price: 55.99,
-			rating: { rate: 4.7, count: 500 },
-			title: "Mens Cotton Jacket",
-			quantity: 1,
-		},
-	]); // used the hooks for simplicity purposes
+  const [cartItems, setCartItems] = useState([
+    // for testing purposes, remove the object and leave the array empty
+    {
+      id: 3,
+      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      price: 55.99,
+      rating: { rate: 4.7, count: 500 },
+      title: "Mens Cotton Jacket",
+      quantity: 1,
+    },
+    {
+      id: 2,
+      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      price: 55.99,
+      rating: { rate: 4.7, count: 500 },
+      title: "Mens Cotton Jacket",
+      quantity: 1,
+    },
+    {
+      id: 1,
+      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      price: 55.99,
+      rating: { rate: 4.7, count: 500 },
+      title: "Mens Cotton Jacket",
+      quantity: 1,
+    },
+  ]); // used the hooks for simplicity purposes
 
-	// a function that can be used both in the cart to increase the quantity and
-	// also in single product component to add the product to cart
-	const addItemHandler = (product) => {
-		const productExist = cartItems.find((item) => item.id === product.id);
-		if (productExist) {
-			setCartItems(
-				cartItems.map((item) =>
-					item.id === product.id
-						? { ...productExist, quantity: productExist.quantity + 1 }
-						: item
-				)
-			);
-		} else {
-			setCartItems([...cartItems, { ...product, quantity: 1 }]);
-		}
-	};
-	const removeItemHanlder = (product) => {
-		const productExist = cartItems.find((item) => item.id === product.id);
-		if (productExist.quantity === 1) {
-			setCartItems(cartItems.filter((item) => item.id !== product.id));
-		} else {
-			setCartItems(
-				cartItems.map((item) =>
-					item.id === product.id
-						? { ...productExist, quantity: productExist.quantity - 1 }
-						: item
-				)
-			);
-		}
-	};
-	return (
-		<div className="App">
-			<Router>
-				<Header />
-				<Routes>
-					<Route index element={<Main />} />
-					<Route path="/shop" element={<ProductsList />} />
-					<Route path="shop/:id" element={<RouteWrapper />} />
-					<Route
-						path="/cart"
-						element={
-							<Cart
-								cartItems={cartItems}
-								addItem={addItemHandler}
-								removeItem={removeItemHanlder}
-							/>
-						}
-					/>
-				</Routes>
-				<Footer />
-			</Router>
-		</div>
-	);
+  // a function that can be used both in the cart to increase the quantity and
+  // also in single product component to add the product to cart
+  const addItemHandler = (product) => {
+    const productExist = cartItems.find((item) => item.id === product.id);
+    if (productExist) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...productExist, quantity: productExist.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+  const removeItemHanlder = (product) => {
+    const productExist = cartItems.find((item) => item.id === product.id);
+    if (productExist.quantity === 1) {
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...productExist, quantity: productExist.quantity - 1 }
+            : item
+        )
+      );
+    }
+  };
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path="/shop" element={<ProductsList />} />
+          <Route path="shop/:id" element={<RouteWrapper />} />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                cartItems={cartItems}
+                addItem={addItemHandler}
+                removeItem={removeItemHanlder}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Cart.css";
+import { Link } from "react-router-dom";
 const Cart = (props) => {
   const { cartItems, addItem, removeItem } = props;
   const subTotal = cartItems.reduce(
@@ -9,23 +10,25 @@ const Cart = (props) => {
   // console.log(subTotal);
   return (
     <div className="cart-container">
+      <h2>Cart</h2>
       {cartItems.length === 0 && (
         <div className="cart-empty">
           <p>Cart is empty</p>
         </div>
       )}
-      <div className="cart-items-container">
-        <h2>Cart</h2>
+      <div className="cart-body">
         <div className="cart-items">
           {cartItems.map((item) => (
             <div key={item.id} className="cart-item-details">
-              <div className="cart-image-title-wrapper">
-                <div className="cart-item-title">{item.title}</div>
+              <div className="cart-image-title">
+                <h4>{item.title}</h4>
                 <img src={item.image} alt={item.title}></img>
               </div>
 
               <div className="cart-price-quantity">
-                <span>{item.quantity}</span> x <span>{item.price}</span> $
+                <div className="cart-price-details">
+                  <span>{item.quantity}</span> x <span>{item.price}</span> $
+                </div>
                 <div className="cart-add-Button">
                   <button onClick={() => addItem(item)}>+</button>
                   <button onClick={() => removeItem(item)}>-</button>
@@ -34,12 +37,25 @@ const Cart = (props) => {
             </div>
           ))}
         </div>
-      </div>
-      <div className="cart-sub-total">
-        {cartItems.length !== 0 && <div> Your Sub-total : {subTotal}</div>}
-        <div className="go-to-payment">
-          <button> Go to payment </button>
+
+        <div className="cart-sub-total">
+          {cartItems.length !== 0 && (
+            <div className="cart-non-empty">
+              <div className="cart-subtotal">
+                Your Sub-total : <span>{subTotal} €</span>
+              </div>
+              <div className="go-to-payment">
+                <button> Go to payment </button>
+              </div>
+              <div className="cart-back-to-list">
+                <Link to={"/shop"}> Keep shopping </Link>
+              </div>
+            </div>
+          )}
         </div>
+      </div>
+      <div className="cart-back-to-list">
+        <Link to={"/shop"}> Keep shopping </Link>
       </div>
     </div>
   );
